d342e1c7105967fd08d7745d220c0fdc1957368f
Stanley Tiu <stanleytiu7@gmail.com>
2019-02-01 21:55:22 -0500

    webpack update

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,12 @@
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
+const {user, pass} = require('../../secrets.js');
+//import {user, pass} from '../../../secrets.js
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL || `postgres://${user}:${pass}@localhost:5432/${databaseName}`,
   {
     logging: false
   }
